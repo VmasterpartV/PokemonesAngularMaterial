1f9111b45d8a912cf68d4932ca4d2c56ca4c53b2
Iván Mota <ivan_mota09@hotmail.com>
2024-03-05 12:19:34 -0600

    dark mode

--- src/app/services/dark-mode/dark-mode.service.ts
+++ src/app/services/dark-mode/dark-mode.service.ts
@@ -8,9 +8,13 @@ export class DarkModeService {
   private isDarkModeSubject = new BehaviorSubject<boolean>(false);
   isDarkMode$ = this.isDarkModeSubject.asObservable();
 
-  constructor() { }
+  constructor() { 
+    document.body.setAttribute('data-bs-theme', 'dark');
+  }
 
   toggleDarkMode() {
     this.isDarkModeSubject.next(!this.isDarkModeSubject.value);
+    const theme = this.isDarkModeSubject.value ? 'dark' : 'light';
+    document.body.setAttribute('data-bs-theme', theme);
   }
 }
